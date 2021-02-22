1756e210af5429b1e09f7d9db72cf178d21b9348
Сергей Диденко <s.didenko@rabota.ru>
2021-02-22 13:08:04 +0300

    fix

--- index.php
+++ index.php
@@ -2,32 +2,32 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+\Workerman\Worker::$daemonize=true;
 $webSocketWorker = new \Workerman\Worker('websocket://0.0.0.0:2346');
 $webSocketWorker->count = 1;
 $client = new WebSocket\Client("wss://ws.finnhub.io?token=c0nakvf48v6v9lphti50");
 
-$webSocketWorker->onConnect = function ($connection) {
+$webSocketWorker->onConnect = function ($connection) use ($client) {
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'AAPL']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'AMZN']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'MSFT']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'ATVI']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'GTHX']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'PLTR']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'EA']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'PYPL']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'CRM']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'TTWO']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'ADBE']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'NFLX']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'GOOG']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'TSLA']));
+    $client->text(json_encode(['type' => 'subscribe', 'symbol' => 'FB']));
+
     echo "New connection\n";
 };
 
 $webSocketWorker->onMessage = function ($connection, $data) use ($webSocketWorker, $client) {
-    $client->text(json_encode([
-        ['type' => 'subscribe', 'symbol' => 'AAPL'],
-        ['type' => 'subscribe', 'symbol' => 'AMZN'],
-        ['type' => 'subscribe', 'symbol' => 'MSFT'],
-        ['type' => 'subscribe', 'symbol' => 'GTHX'],
-        ['type' => 'subscribe', 'symbol' => 'PLTR'],
-        ['type' => 'subscribe', 'symbol' => 'ATVI'],
-        ['type' => 'subscribe', 'symbol' => 'EA'],
-        ['type' => 'subscribe', 'symbol' => 'PYPL'],
-        ['type' => 'subscribe', 'symbol' => 'CRM'],
-        ['type' => 'subscribe', 'symbol' => 'TTWO'],
-        ['type' => 'subscribe', 'symbol' => 'ADBE'],
-        ['type' => 'subscribe', 'symbol' => 'NFLX'],
-        ['type' => 'subscribe', 'symbol' => 'GOOG'],
-        ['type' => 'subscribe', 'symbol' => 'TSLA'],
-        ['type' => 'subscribe', 'symbol' => 'FB'],
-    ]));
     $connection->send($client->receive());
 };
 
