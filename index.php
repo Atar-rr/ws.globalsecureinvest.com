c9eb61f31ef69d6a2b7049c569b8c7bd7cddfe21
Сергей Диденко <s.didenko@rabota.ru>
2021-02-26 01:31:11 +0300

    fix

--- index.php
+++ index.php
@@ -1,21 +1,32 @@
 <?php
 
-use WebSocket\Client;
+use GuzzleHttp\Client;
 use Workerman\Connection\TcpConnection;
 use Workerman\Lib\Timer;
+use WSSC\Components\ClientConfig;
+use WSSC\WebSocketClient;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$httpClient = new \GuzzleHttp\Client();
+const FINHUB_URI = 'wss://ws.finnhub.io?token=c0nakvf48v6v9lphti50';
+const SOCKET_NAME = 'websocket://45.90.33.104:2346';
 
-//получаем данные о том, какие акции нужно получать
+const SUBSCRIBE = 'subscribe';
+const UNSUBSCRIBE = 'unsubscribe';
 
+const FIELD_SYMBOL = 'symbol';
+const FIELD_TYPE   = 'type';
+const FIELD_DATA   = 'data';
+const FIELD_S      = 's';
+
+$httpClient = new Client();
+
+//запрашиваем данные о том, какие акции нужно получать
 try {
-    $httpClient = new \GuzzleHttp\Client();
+    $httpClient = new Client();
     $data = $httpClient->request('GET', '');
     $symbols = json_decode($data, true);
 } catch (\Exception $e) {
-    #TODO дополнить
     $symbols = ['AAPL', 'FB', 'TSLA', 'PLTR', 'AMZN', 'ATVI', 'MSFT'];
 }
 
@@ -31,53 +42,53 @@ $context = [
 \Workerman\Worker::$daemonize=true;
 
 //создаем WebSocket
-$webSocketWorker = new \Workerman\Worker('websocket://45.90.33.104:2346', $context);
-$webSocketWorker->count = 4;
+$webSocketWorker = new \Workerman\Worker(SOCKET_NAME, $context);
 $webSocketWorker->transport = 'ssl';
 
+$config = new ClientConfig();
+$config->setFragmentSize(20000);
+$config->setTimeout(50);
 
-$client = new Client("wss://ws.finnhub.io?token=c0nakvf48v6v9lphti50");
-$client->setFragmentSize(30000);
-$client->setTimeout(50);
+$client = new WebSocketClient(FINHUB_URI, $config);
 
 $webSocketWorker->onConnect = function ($connection) use ($client, $symbols) {
     echo "New connection\n";
-    foreach ($symbols as $symbol) {
-        $client->text(json_encode(['type' => 'subscribe', 'symbol' => $symbol]));
-    }
-
-    TcpConnection::$defaultMaxSendBufferSize = 10485760;
 };
 
 $webSocketWorker->onMessage = function ($connection, $message) use ($webSocketWorker, $client) {
+    $time_interval = 1.1;
 
-    $time_interval = 1.3;
     Timer::add($time_interval, function() use ($connection, $client, $message)
     {
         $result = [];
         $resultFinhub = json_decode($client->receive(), true);
 
-        $returnSymb = json_decode($message, true);
-        if (!empty($returnSymb) && !empty($resultFinhub)) {
-            $result = array_filter($resultFinhub['data'], function ($item) use ($returnSymb) {
-                return in_array($item['s'], $returnSymb);
+        $returnSymbols = json_decode($message, true);
+        if (!empty($returnSymbols) && !empty($resultFinhub) && isset($resultFinhub[FIELD_DATA])) {
+            $result = array_filter($resultFinhub[FIELD_DATA], function ($item) use ($returnSymbols) {
+                return in_array($item[FIELD_S], $returnSymbols);
             });
         }
-        $connection->send(empty($returnSymb) === false ? json_encode($result, JSON_UNESCAPED_UNICODE) : json_encode($resultFinhub, JSON_UNESCAPED_UNICODE));
+
+        $connection->send(empty($returnSymbols) === false ? json_encode($result, JSON_UNESCAPED_UNICODE) : json_encode($resultFinhub, JSON_UNESCAPED_UNICODE));
     });
 };
 
 $webSocketWorker->onClose = function () use ($client, $symbols) {
+    echo "Connection closed\n";
+};
+
+$webSocketWorker->onWorkerStart = function () use ($client, $symbols) {
     foreach ($symbols as $symbol) {
-        $client->text(json_encode(['type' => 'unsubscribe', 'symbol' => $symbol]));
+        $client->send(json_encode([FIELD_TYPE => SUBSCRIBE, FIELD_SYMBOL => $symbol]));
     }
 
-    echo "Connection closed\n";
+    TcpConnection::$defaultMaxSendBufferSize = 10485760;
 };
 
 $webSocketWorker->onWorkerStop = function () use ($client, $symbols) {
     foreach ($symbols as $symbol) {
-        $client->text(json_encode(['type' => 'unsubscribe', 'symbol' => $symbol]));
+        $client->send(json_encode([FIELD_TYPE => UNSUBSCRIBE, FIELD_SYMBOL => $symbol]));
     }
 };
 
